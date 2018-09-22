1220ce51d8d68793f895d3d841cf93e2d3851fbc
Russell O'Heney <russello@unity3d.com>
2018-09-21 17:11:36 -0700

    Changes for PR.

--- com.unity.render-pipelines.high-definition/Editor/Material/Lit/ShaderGraph/HDLitUI.cs
+++ com.unity.render-pipelines.high-definition/Editor/Material/Lit/ShaderGraph/HDLitUI.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.HDPipeline;
 
 namespace UnityEditor.ShaderGraph
 {
@@ -7,12 +8,24 @@ namespace UnityEditor.ShaderGraph
     {
         public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] props)
         {
-			materialEditor.PropertiesDefaultGUI(props);
+            materialEditor.PropertiesDefaultGUI(props);
             if (materialEditor.EmissionEnabledProperty())
             {
                 // Use the overload version of this function once the following PR is merged: Pull request #74105
                 materialEditor.LightmapEmissionFlagsProperty(MaterialEditor.kMiniTextureFieldLabelIndentLevel, true);
                 //materialEditor.LightmapEmissionFlagsProperty(MaterialEditor.kMiniTextureFieldLabelIndentLevel, true, true);
+            }
+
+            var material = (Material)materialEditor.target;
+            if (material != null)
+            {
+                bool enabled = material.GetShaderPassEnabled(HDShaderPassNames.s_MotionVectorsStr);
+                EditorGUI.BeginChangeCheck();
+                enabled = EditorGUILayout.Toggle("Enable Motion Vector For Vertex Animation", enabled);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    material.SetShaderPassEnabled(HDShaderPassNames.s_MotionVectorsStr, enabled);
+                }
             }
         }
     }
